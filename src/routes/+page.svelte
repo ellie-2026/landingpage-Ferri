628898550f62486b29435d15ea6368498ae8d33c
ellie-2026 <elisa1.ferri@mail.polimi.it>
2026-04-25 17:05:22 +0200

    immagini

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,63 @@
 <script>
-    import Filter from "$lib/components/Filter.svelte";
-    import Link from "$lib/components/Link.svelte";
-    import Project from "$lib/components/Project.svelte";
+  import Filter from "$lib/components/Filter.svelte";
+  import Project from "$lib/components/Project.svelte";
 
-    const { data } = $props();
+  const { data } = $props();
 
-    let currentYear = $state(data.years[0].number);
-    let projects = $derived.by(() => {
+  // Stato per l'anno selezionato (inizializzato col primo anno disponibile)
+  let currentYear = $state(data.years[0].number);
+
+  // Derivata che filtra i progetti in base all'anno selezionato
+  let projects = $derived.by(() => {
     return data.years.find((year) => {
-        return year.number == currentYear;
-        }).projects;
-    });
+      return year.number == currentYear;
+    }).projects;
+  });
 </script>
 
 <section class="safe-area hero">
-    <h1>
-        I don’t wait for perfection; I find it in the in-between. 
-        I don’t stage anything, I capture them as they appear. 
-        These are my journeys, told through my photographs. 
-        Each section is a new journey.
-    </h1>
+  <h1>
+    This archive collects the best outcomes of the Web Design Laboratory in
+    Communication Design, Politecnico di Milano. The laboratory is directed by
+    Umberto Tolino, Christian Mazzoleni, Francesco di Gioia and Tommaso Negri
+    with love.
+  </h1>
 </section>
 
 <nav class="safe-area filters">
-    {#each data.years as year}
-        <Filter bind:group={currentYear} value={year.number} />
-    {/each}
+  {#each data.years as year}
+    <Filter bind:group={currentYear} value={year.number} />
+  {/each}
 </nav>
 
 <section class="safe-area projects">
-    {#each projects as project}
-        <Project data={project.data} />
-    {/each}
+  {#each projects as project}
+    <Project data={project.data} />
+  {/each}
 </section>
 
 <style>
-    .hero {
-        padding-block: var(--size-11);
+  .hero {
+    padding-block: var(--size-11);
 
-        h1 {
-            font-size: var(--size-7);
-            max-width: 35ch;
-        }
+    h1 {
+      font-size: var(--size-7);
+      max-width: 35ch;
     }
+  }
 
-    .filters {
-        display: flex;
-        align-items: center;
-        gap: var(--size-5);
-    }
-    
+  .filters {
+    display: flex;
+    align-items: center;
+    gap: var(--size-5);
+    /* Aggiunto un po' di padding per staccarlo dai progetti */
+    padding-block: var(--size-4);
+  }
+
+  .projects {
+    padding-block: var(--size-7);
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: var(--size-5);
+  }
 </style>
